b377af6986f0372bb0707512e52a7b54f7d323c2
chenkby <99912250@qq.com>
2017-02-22 15:06:12 +0800

    修复没有启用prettyUrl时导致的访问错误

--- Region.php
+++ Region.php
@@ -1,5 +1,6 @@
 <?php
 namespace chenkby\region;
+
 use yii\base\InvalidParamException;
 use yii\base\Widget;
 use yii\helpers\ArrayHelper;
@@ -7,7 +8,7 @@ use yii\helpers\Html;
 
 class Region extends Widget
 {
-    public $model=null;
+    public $model = null;
 
     /**
      * @var string 此属性不用处理
@@ -17,17 +18,17 @@ class Region extends Widget
     /**
      * @var array 省份配置
      */
-    public $province=[];
+    public $province = [];
 
     /**
      * @var array 城市配置
      */
-    public $city=[];
+    public $city = [];
 
     /**
      * @var array 县/区配置
      */
-    public $district=[];
+    public $district = [];
 
     /**
      * @var mixed 数据源
@@ -36,35 +37,46 @@ class Region extends Widget
 
     public function init()
     {
-        if(!$this->model)throw new InvalidParamException('model不能为null!');
+        if (!$this->model) {
+            throw new InvalidParamException('model不能为null!');
+        }
 
-        if(empty($this->province) || empty($this->city))throw new InvalidParamException('province和city不能为空！');
+        if (empty($this->province) || empty($this->city)) {
+            throw new InvalidParamException('province和city不能为空！');
+        }
 
-        $cityId=Html::getInputId($this->model,$this->city['attribute']);
-        if(empty($this->city['options']['prompt']))$this->city['options']['prompt']='选择城市';
-        $cityDefault=Html::renderSelectOptions('city',[''=>$this->city['options']['prompt']]);
+        $cityId = Html::getInputId($this->model, $this->city['attribute']);
+        if (empty($this->city['options']['prompt'])) {
+            $this->city['options']['prompt'] = '选择城市';
+        }
+        $cityDefault = Html::renderSelectOptions('city', ['' => $this->city['options']['prompt']]);
 
-        if(!empty($this->district)){
-            if(empty($this->district['options']['prompt']))$this->district['options']['prompt']='选择县/区';
-            $districtId=Html::getInputId($this->model,$this->district['attribute']);
-            $districtDefault=Html::renderSelectOptions('district',[''=>$this->district['options']['prompt']]);
-            $this->city['options']=ArrayHelper::merge($this->city['options'],[
-                'onchange'=>"
-                if($(this).val()!=''){
-                    $.get('{$this->url}?parent_id='+$(this).val(),function(data){
-                        $('#{$districtId}').html('{$districtDefault}'+data);
-                    })
-                }else{
-                    $('#{$districtId}').html('{$districtDefault}');
-                }
-            "
+        $joinChar = strripos($this->url, '?') ? '&' : '?';
+        $url = $this->url . $joinChar;
+
+        if (!empty($this->district)) {
+            if (empty($this->district['options']['prompt'])) {
+                $this->district['options']['prompt'] = '选择县/区';
+            }
+            $districtId = Html::getInputId($this->model, $this->district['attribute']);
+            $districtDefault = Html::renderSelectOptions('district', ['' => $this->district['options']['prompt']]);
+            $this->city['options'] = ArrayHelper::merge($this->city['options'], [
+                'onchange' => "
+                    if($(this).val() != ''){
+                        $.get('{$url}parent_id='+$(this).val(), function(data) {
+                            $('#{$districtId}').html('{$districtDefault}'+data);
+                        })
+                    }else{
+                        $('#{$districtId}').html('{$districtDefault}');
+                    }
+                "
             ]);
         }
 
-        $this->province['options']=ArrayHelper::merge($this->province['options'],[
-            'onchange'=>"
+        $this->province['options'] = ArrayHelper::merge($this->province['options'], [
+            'onchange' => "
                 if($(this).val()!=''){
-                    $.get('{$this->url}?parent_id='+$(this).val(),function(data){
+                    $.get('{$url}parent_id='+$(this).val(), function(data) {
                         $('#{$cityId}').html('{$cityDefault}'+data);
                     })
                 }else{
@@ -78,12 +90,15 @@ class Region extends Widget
 
     public function run()
     {
-        $output[]=Html::activeDropDownList($this->model,$this->province['attribute'],$this->province['items'],$this->province['options']);
-        $output[]=Html::activeDropDownList($this->model,$this->city['attribute'],$this->city['items'],$this->city['options']);
-        if(!empty($this->district)){
-            $output[]=Html::activeDropDownList($this->model,$this->district['attribute'],$this->district['items'],$this->district['options']);
+        $output[] = Html::activeDropDownList($this->model, $this->province['attribute'], $this->province['items'],
+            $this->province['options']);
+        $output[] = Html::activeDropDownList($this->model, $this->city['attribute'], $this->city['items'],
+            $this->city['options']);
+        if (!empty($this->district)) {
+            $output[] = Html::activeDropDownList($this->model, $this->district['attribute'], $this->district['items'],
+                $this->district['options']);
         }
-        return @implode("\n",$output);
+        return @implode("\n", $output);
     }
 
 }
